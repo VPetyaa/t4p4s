551104fd78ac9738ba0c24db7c324dbf77792703
Peter Voros <vpetya@mensa.hu>
2021-02-03 09:50:02 +0000

    Docker iperf script updated

--- start_docker_iperf.sh
+++ start_docker_iperf.sh
@@ -1,5 +1,6 @@
 tmux new-session -d -s main ;
-tmux send-keys -t main "./t4p4s.sh :$1" C-m
+tmux send-keys -t main "./t4p4s.sh :$1 verbose" C-m
+#tmux send-keys -t main "./t4p4s.sh :$1 verbose > /tmp/t4p4s.log& tail -f /tmp/t4p4s.log" C-m
 tmux split-window -v ;
 tmux select-pane -2
 
@@ -8,5 +9,6 @@ tmux select-pane -L
 tmux send-keys -t main 'sudo ip netns exec red iperf -s 10.10.10.2' C-m
 tmux select-pane -R
 tmux send-keys -t main 'sudo ip netns exec blue iperf -c 10.10.10.2' C-m
+#tmux send-keys -t main 'sudo ip netns exec blue iperf -c 10.10.10.2 > /tmp/iperf.log& tail -f /tmp/iperf.log' C-m
 tmux attach-session -d -t main 
 
